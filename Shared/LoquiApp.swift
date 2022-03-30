f8cbb82f3b2fecce44425ef6712767e14b52f308
Alfredo Cabanas <02cabanas@gmail.com>
2022-03-30 15:37:39 -0400

    Initial Commit

---
+++ Shared/LoquiApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoquiApp.swift
+//  Shared
+//
+//  Created by alfredo cabanas on 3/30/22.
+//
+
+import SwiftUI
+
+@main
+struct LoquiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
